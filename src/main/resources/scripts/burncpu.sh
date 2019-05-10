47523279d36a424c66a7032c22aa2692fe7ea005
levin ding <levin_ding@trendmicro.com.cn>
2019-05-10 11:02:43 +0800

    update the burncpu script

--- src/main/resources/scripts/burncpu.sh
+++ src/main/resources/scripts/burncpu.sh
@@ -8,11 +8,10 @@ while true;
 done
 EOF
 
-echo 'burncpu' > /tmp/test.log
-sleep 120
 
 # 32 parallel 100% CPU tasks should hit even the biggest EC2 instances
 for i in {1..32}
 do
+    echo $i >> /tmp/test.log
     nohup /bin/bash /tmp/infiniteburn.sh > /tmp/nohup.log 2>&1 &
 done
