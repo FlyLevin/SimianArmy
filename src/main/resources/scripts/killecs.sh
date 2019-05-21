58cf6a91385d653609be5c063aa249cbd822b139
levin ding <levin_ding@trendmicro.com.cn>
2019-05-21 17:13:03 +0800

    update the kill ecs script

--- src/main/resources/scripts/killecs.sh
+++ src/main/resources/scripts/killecs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker ps|grep test|awk -F '{print $1}' > /tmp/container-id
+docker ps|grep 'ecs-task'|awk -F ' ' '{print $1}' > /tmp/container-id
 killid=`shuf -n 1 /tmp/container-id`
 docker kill $killid
